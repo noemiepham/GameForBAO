1ce5a5c908f706cfcb4e6927466918852b0a2293
Noémie Huynh <huynhtieuquy@gmail.com>
2023-11-14 13:48:14 +0100

    upate no zoom

--- index.js
+++ index.js
@@ -8,3 +8,9 @@ function showPage() {
   document.getElementById("loader").style.display = "none";
   document.getElementById("myDiv").style.display = "block";
 }
+
+window.addEventListener('touchstart', function(event) {
+  if (event.touches.length > 1) {
+     event.preventDefault();
+  }
+}, { passive: false });
